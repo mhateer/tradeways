0426d5b0e4d6d88c6f61598a5ca758c01e9c9f7c
Muhateer Muhammad <bamsaibey@outlook.com>
2025-06-12 20:38:58 +0500

    updated design picture dimension correction

--- src/components/OnlineStores/OnlineStores.jsx
+++ src/components/OnlineStores/OnlineStores.jsx
@@ -28,6 +28,7 @@ const OnlineStores = () => {
       <div className="heading">
         <h1>We Are Your Online <br /> Supply & Distribution Partner</h1>
       </div>
+
       <div className="paragraph">
         <p>
           Logrem ipsum dolor sit amet, consectetur adipiscing elit. Nam ligula metus, elementum vel orci in, luctus finibus nisi. Sed <br />
@@ -35,35 +36,53 @@ const OnlineStores = () => {
           vestibulum justo ut sagittis.
         </p>
       </div>
+
       <div className="subHeading">
         <h2>Visit Our Online Stores At</h2>
       </div>
 
       <div className="store-logos">
-        <div className={`store-box amazon ${visibleContainers.includes(0) ? "show" : ""}`}>
-          <img src={amazon} alt="Amazon FBA" />
-        </div>
+        <a href={process.env.REACT_APP_AMAZON_URL} target="_blank" rel="noopener noreferrer">
+          <div className={`store-box amazon ${visibleContainers.includes(0) ? "show" : ""}`}>
+            <img className="amazon-img" src={amazon} alt="Amazon FBA" />
+          </div>
+        </a>
+
         <div className="center">
           <div className="top">
-            <div className={`store-box target ${visibleContainers.includes(2) ? "show" : ""}`}>
-              <img src={target} alt="Target" />
-            </div>
-            <div className={`store-box etsy ${visibleContainers.includes(3) ? "show" : ""}`}>
-              <img src={etsy} alt="Etsy" />
-            </div>
+            <a href={process.env.REACT_APP_TARGET_URL} target="_blank" rel="noopener noreferrer">
+              <div className={`store-box target ${visibleContainers.includes(2) ? "show" : ""}`}>
+                <img className="target-img" src={target} alt="Target" />
+              </div>
+            </a>
+
+            <a href={process.env.REACT_APP_ETSY_URL} target="_blank" rel="noopener noreferrer">
+              <div className={`store-box etsy ${visibleContainers.includes(3) ? "show" : ""}`}>
+                <img className="etsy-img" src={etsy} alt="Etsy" />
+              </div>
+            </a>
           </div>
+
           <div className="bottom">
-            <div className={`store-box tiktok ${visibleContainers.includes(4) ? "show" : ""}`}>
-              <img src={tiktok} alt="TikTok Shop" />
-            </div>
-            <div className={`store-box ebay ${visibleContainers.includes(5) ? "show" : ""}`}>
-              <img src={ebay} alt="eBay" />
-            </div>
+            <a href={process.env.REACT_APP_TIKTOK_URL} target="_blank" rel="noopener noreferrer">
+              <div className={`store-box tiktok ${visibleContainers.includes(4) ? "show" : ""}`}>
+                <img className="tiktok-img" src={tiktok} alt="TikTok Shop" />
+              </div>
+            </a>
+
+            <a href={process.env.REACT_APP_EBAY_URL} target="_blank" rel="noopener noreferrer">
+              <div className={`store-box ebay ${visibleContainers.includes(5) ? "show" : ""}`}>
+                <img className="ebay-img" src={ebay} alt="eBay" />
+              </div>
+            </a>
           </div>
         </div>
-        <div className={`store-box walmart ${visibleContainers.includes(1) ? "show" : ""}`}>
-          <img src={walmart} alt="Walmart (Coming Soon)" />
-        </div>
+
+        <a href={process.env.REACT_APP_WALMART_URL} target="_blank" rel="noopener noreferrer">
+          <div className={`store-box walmart ${visibleContainers.includes(1) ? "show" : ""}`}>
+            <img className="waltmart-img" src={walmart} alt="Walmart (Coming Soon)" />
+          </div>
+        </a>
       </div>
     </section>
   );
